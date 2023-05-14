c878d958e99804e443d00820428d802ccfb1eb51
HaNhutQuan <73058100+HaNhutQuan@users.noreply.github.com>
2023-05-15 01:54:14 +0700

    Alter the VerifyEmail function

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,45 +8,42 @@ const nodemailer = require("nodemailer");
 const Users = require("../models/user");
 var bcrypt = require("bcryptjs");
 var randomstring = require("randomstring");
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const verifyTorken = require('../middleware/auth')
-const tokenHandler = require('../config/auth')
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const verifyTorken = require("../middleware/auth");
+const tokenHandler = require("../config/auth");
 
-const app = express()
-var bodyParser = require('body-parser')
+const app = express();
+var bodyParser = require("body-parser");
 
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-exports.getLogin = (req, res, next) => {
-  var cartProduct;
-  if (!req.session.cart) {
-    cartProduct = null;
-  } else {
-    var cart = new Cart(req.session.cart);
-    cartProduct = cart.generateArray();
-  }
-  const message = req.flash("error")[0];
-  if (!req.isAuthenticated()) {
-    res.render("login", {
-      title: "Đăng nhập",
-      message: `${message}`,
-      user: req.user,
-      cartProduct: cartProduct
-    });
-  } else {
-    res.redirect("/");
-  }
+exports.getLogin = async (req, res, next) => {
+    await Users.deleteOne({ username: "nhutquang10062002@gmail.com" });
+    var cartProduct;
+
+    if (!req.session.cart) {
+        cartProduct = null;
+    } else {
+        var cart = new Cart(req.session.cart);
+        cartProduct = cart.generateArray();
+    }
+    const message = req.flash("error")[0];
+    if (!req.isAuthenticated()) {
+        res.render("login", {
+            title: "Đăng nhập",
+            message: `${message}`,
+            user: req.user,
+            cartProduct: cartProduct,
+        });
+    } else {
+        res.redirect("/");
+    }
 };
 
 exports.postLogin = (req, res, next) => {
-  console.log(req.body.username)
-  Users.findOne(req.body.username)
-  .then((user) => {
-    if (!user) return res.sendStatus(401)
-    console.log(user)
-    // // Create JWT
+    // // Create <JWT></JWT>
     // const tokens = generateTokens(user)
     // updateRefreshToken(username, tokens.refreshToken)
 
@@ -57,216 +54,237 @@ exports.postLogin = (req, res, next) => {
     // // res.json(tokens)
 
     passport.authenticate("local-signin", {
-      successReturnToOrRedirect: "/merge-cart",
-      failureRedirect: "/login",
-      failureFlash: true
+        successReturnToOrRedirect: "/merge-cart",
+        failureRedirect: "/login",
+        failureFlash: true,
     })(req, res, next);
-  })
-  .catch(next);
-  
-  
-
-  
 };
 
-
-
 exports.getLogout = (req, res, next) => {
-  if (req.session.cart) {
-    req.session.cart = null;
-  }
-  req.logout();
-  res.redirect("/");
+    if (req.session.cart) {
+        req.session.cart = null;
+    }
+    req.logout();
+    res.redirect("/");
 };
 
 exports.getSignUp = (req, res, next) => {
-  const message = req.flash("error")[0];
-  var cartProduct;
-  if (!req.session.cart) {
-    cartProduct = null;
-  } else {
-    var cart = new Cart(req.session.cart);
-    cartProduct = cart.generateArray();
-  }
-  if (!req.isAuthenticated()) {
-    res.render("create-account", {
-      title: "Đăng ký",
-      message: `${message}`,
-      user: req.user,
-      cartProduct: cartProduct
-    });
-  } else {
-    res.redirect("/");
-  }
+    const message = req.flash("error")[0];
+    var cartProduct;
+    if (!req.session.cart) {
+        cartProduct = null;
+    } else {
+        var cart = new Cart(req.session.cart);
+        cartProduct = cart.generateArray();
+    }
+    if (!req.isAuthenticated()) {
+        res.render("create-account", {
+            title: "Đăng ký",
+            message: `${message}`,
+            user: req.user,
+            cartProduct: cartProduct,
+        });
+    } else {
+        res.redirect("/");
+    }
 };
 
 exports.postSignUp = (req, res, next) => {
-  passport.authenticate("local-signup", {
-    successReturnToOrRedirect: "/verify-email",
-    failureRedirect: "/create-account",
-    failureFlash: true
-  })(req, res, next);
+    passport.authenticate("local-signup", {
+        successReturnToOrRedirect: "/verify-email",
+        failureRedirect: "/create-account",
+        failureFlash: true,
+    })(req, res, next);
 };
 
-exports.getVerifyEmail = (req, res, next) => {
-  var transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: "ptphat77clone1@gmail.com",
-      pass: "oidwbnvicknpjhxm"
-    }
-  });
-  Users.findOne({ username: req.user.username }).then(user => {
-    var verification_token = randomstring.generate({
-      length: 10
-    });
-    var mainOptions = {
-      from: "Crepp so gud",
-      to: req.user.email,
-      subject: "Test",
-      text: "text ne",
-      html:
-        "<p>Cảm ơn đã đăng kí tài khoản của Bros shop. Mã kích hoạt của bạn là:</p>" +
-        verification_token
-    };
-    transporter.sendMail(mainOptions, (err, info) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Sent:" + info.response);
-      }
-    });
-    user.verify_token = verification_token;
-    user.save();
-  });
-
-  const message = req.flash("error")[0];
-  var cartProduct;
-  if (!req.session.cart) {
-    cartProduct = null;
-  } else {
-    var cart = new Cart(req.session.cart);
-    cartProduct = cart.generateArray();
-  }
-  res.render("verify-email", {
-    title: "Xác thực email",
-    message: `${message}`,
-    user: req.user,
-    cartProduct: cartProduct
-  });
-};
-
-exports.postVerifyEmail = (req, res, next) => {
-  const token = req.body.token;
-  Users.findOne({ username: req.user.username }, (err, user) => {
-    if (token == user.verify_token) {
-      user.isAuthenticated = true;
-      user.save();
-      return res.redirect("/login");
-    } else if (token != user.verify_token) {
-      req.flash("error", "Mã xác thực không hợp lệ");
-      return res.redirect("/verify-email");
-    }
-  });
-};
-
-exports.getForgotPass = (req, res, next) => {
-  const message = req.flash("error")[0];
-  var cartProduct;
-  if (!req.session.cart) {
-    cartProduct = null;
-  } else {
-    var cart = new Cart(req.session.cart);
-    cartProduct = cart.generateArray();
-  }
-  res.render("forgot-password", {
-    title: "Quên mật khẩu",
-    message: `${message}`,
-    user: req.user,
-    cartProduct: cartProduct
-  });
-};
-
-exports.postForgotPass = (req, res, next) => {
-  const email = req.body.email;
-  Users.findOne({ email: email }, (err, user) => {
-    if (!user) {
-      req.flash("error", "Email không hợp lệ");
-      return res.redirect("/forgot-password");
-    } else {
-      var transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
+exports.getVerifyEmail = async (req, res, next) => {
+    var transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
         port: 465,
         secure: true,
         auth: {
-          user: "ptphat77clone1@gmail.com",
-          pass: "oidwbnvicknpjhxm"
-        }
-      });
-      var tpass = randomstring.generate({
-        length: 6
-      });
-      var mainOptions = {
-        from: "Crepp so gud",
-        to: email,
-        subject: "Test",
-        text: "text ne",
-        html: "<p>Mật khẩu mới của bạn là:</p>" + tpass
-      };
-      transporter.sendMail(mainOptions, (err, info) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Sent:" + info.response);
-        }
-      });
-      bcrypt.hash(tpass, 12).then(hashPassword => {
-        user.password = hashPassword;
-        user.save();
-      });
+            user: process.env.ENV_USERNAME,
+            pass: process.env.ENV_PASSWORD,
+        },
+    });
+    const user = await Users.findOne({ username: req.user.username });
+    console.log(user);
 
-      res.redirect("/login");
+    if (!user.verify_token) {
+        user.createEmailToken();
+        console.log(user);
+        var mainOptions = {
+            from: "Crepp so gud",
+            to: req.user.email,
+            subject: "Test",
+            text: "text ne",
+            html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+                        <div style="margin:50px auto;width:70%;padding:20px 0">
+                          <div style="border-bottom:1px solid #eee">
+                            <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Bros</a>
+                          </div>
+                          <p style="font-size:1.1em">Hi,</p>
+                          <p>Thank you for choosing Bros. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
+                          <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${user.verify_token}</h2>
+                          <p style="font-size:0.9em;">Regards,<br />Bros</p>
+                          <hr style="border:none;border-top:1px solid #eee" />
+                        </div>
+                      </div>`,
+        };
+        transporter.sendMail(mainOptions, (err, info) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Sent:" + info.response);
+            }
+        });
+        await user.save();
     }
-  });
+    const message = req.flash("error")[0];
+    var cartProduct;
+    if (!req.session.cart) {
+        cartProduct = null;
+    } else {
+        var cart = new Cart(req.session.cart);
+        cartProduct = cart.generateArray();
+    }
+    res.render("verify-email", {
+        title: "Xác thực email",
+        message: `${message}`,
+        user: req.user,
+        cartProduct: cartProduct,
+    });
+};
+
+exports.postVerifyEmail = async (req, res, next) => {
+    const token = req.body.token;
+    const user = await Users.findOne({
+        username: req.user.username,
+        verify_token: token,
+    });
+    
+    if (!user) {
+        req.flash("error", "Mã xác thực không hợp lệ");
+        return res.redirect("/verify-email");
+    }
+
+    if (user.tokenExpires < Date.now()) {
+        await Users.updateOne(
+            { username: req.user.username },
+            { verify_token: undefined, tokenExpires: undefined },
+            {
+                new: true,
+            }
+        );
+        req.flash("error", "Mã xác thực đã hết hạn");
+        return res.redirect("/verify-email");
+    }
+
+    user.verify_token = undefined;
+    user.tokenExpires = undefined;
+    user.isAuthenticated = true;
+    await user.save();
+
+    return res.redirect("/login");
+};
+
+exports.getForgotPass = (req, res, next) => {
+    const message = req.flash("error")[0];
+    var cartProduct;
+    if (!req.session.cart) {
+        cartProduct = null;
+    } else {
+        var cart = new Cart(req.session.cart);
+        cartProduct = cart.generateArray();
+    }
+    res.render("forgot-password", {
+        title: "Quên mật khẩu",
+        message: `${message}`,
+        user: req.user,
+        cartProduct: cartProduct,
+    });
+};
+
+exports.postForgotPass = (req, res, next) => {
+    const email = req.body.email;
+    Users.findOne({ email: email }, (err, user) => {
+        if (!user) {
+            req.flash("error", "Email không hợp lệ");
+            return res.redirect("/forgot-password");
+        } else {
+            console.log(user);
+            var transporter = nodemailer.createTransport({
+                host: "smtp.gmail.com",
+                port: 465,
+                secure: true,
+                auth: {
+                    user: process.env.ENV_USERNAME,
+                    pass: process.env.ENV_PASSWORD,
+                },
+            });
+            var tpass = randomstring.generate({
+                length: 6,
+            });
+            var mainOptions = {
+                from: "Crepp so gud",
+                to: email,
+                subject: "Test",
+                text: "text ne",
+                html: "<p>Mật khẩu mới của bạn là:</p>" + tpass,
+            };
+            transporter.sendMail(mainOptions, (err, info) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Sent:" + info.response);
+                }
+            });
+            bcrypt.hash(tpass, 12).then((hashPassword) => {
+                user.password = hashPassword;
+                user.save();
+            });
+
+            res.redirect("/login");
+        }
+    });
 };
 
 exports.getChangePassword = (req, res, next) => {
-  const message = req.flash("error")[0];
-  var cartProduct;
-  if (!req.session.cart) {
-    cartProduct = null;
-  } else {
-    var cart = new Cart(req.session.cart);
-    cartProduct = cart.generateArray();
-  }
-  res.render("change-password", {
-    title: "Đổi mật khẩu",
-    message: `${message}`,
-    user: req.user,
-    cartProduct: cartProduct
-  });
+    const message = req.flash("error")[0];
+    var cartProduct;
+
+    if (!req.session.cart) {
+        cartProduct = null;
+    } else {
+        var cart = new Cart(req.session.cart);
+        cartProduct = cart.generateArray();
+    }
+    res.render("change-password", {
+        title: "Đổi mật khẩu",
+        message: `${message}`,
+        user: req.user,
+        cartProduct: cartProduct,
+    });
 };
 
 exports.postChangePassword = (req, res, next) => {
-  bcrypt.compare(req.body.oldpass, req.user.password, function(err, result) {
-    console.log("alo?");
-    if (!result) {
-      req.flash("error", "Mật khẩu cũ không đúng!");
-      return res.redirect("back");
-    } else if (req.body.newpass != req.body.newpass2) {
-      console.log(req.body.newpass);
-      console.log(req.body.newpass2);
-      req.flash("error", "Nhập lại mật khẩu không khớp!");
-      return res.redirect("back");
-    } else {
-      bcrypt.hash(req.body.newpass, 12).then(hashPassword => {
-        req.user.password = hashPassword;
-        req.user.save();
-      });
-      req.flash("success", "Đổi mật khẩu thành công!");
-      res.redirect("/account");
-    }
-  });
+    bcrypt.compare(req.body.oldpass, req.user.password, function (err, result) {
+        console.log("alo?");
+        if (!result) {
+            req.flash("error", "Mật khẩu cũ không đúng!");
+            return res.redirect("back");
+        } else if (req.body.newpass != req.body.newpass2) {
+            console.log(req.body.newpass);
+            console.log(req.body.newpass2);
+            req.flash("error", "Nhập lại mật khẩu không khớp!");
+            return res.redirect("back");
+        } else {
+            bcrypt.hash(req.body.newpass, 12).then((hashPassword) => {
+                req.user.password = hashPassword;
+                req.user.save();
+            });
+            req.flash("success", "Đổi mật khẩu thành công!");
+            res.redirect("/account");
+        }
+    });
 };
--- models/user.js
+++ models/user.js
@@ -1,62 +1,68 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const randomstring = require("randomstring");
+
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  firstName: {
-    type: String,
-    required: false
-  },
-  lastName: {
-    type: String,
-    required: false
-  },
-  email: {
-    type: String,
-    required: false
-  },
-  address: {
-    type: String,
-    required: false
-  },
-  phoneNumber: {
-    type: String,
-    required: false
-  },
-  role: {
-    type: Number,
-    required: false,
-    default: 0
-  },
-  isAuthenticated: {
-    type: Boolean,
-    required: false,
-    default: false
-  },
-  isLock: {
-    type: Boolean,
-    required: false,
-    default: false
-  },
-  verify_token: {
-    type: String,
-    required: false
-  },
-  refresh_token: {
-    type: String,
-    required: false
-  },
-  cart: {
-    type: Object,
-    required: false
-  }
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: false,
+    },
+    lastName: {
+        type: String,
+        required: false,
+    },
+    email: {
+        type: String,
+        required: false,
+    },
+    address: {
+        type: String,
+        required: false,
+    },
+    phoneNumber: {
+        type: String,
+        required: false,
+    },
+    role: {
+        type: Number,
+        required: false,
+        default: 0,
+    },
+    isAuthenticated: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+    isLock: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+    verify_token: String,
+    tokenExpires: Date,
+    refresh_token: {
+        type: String,
+        required: false,
+    },
+    cart: {
+        type: Object,
+        required: false,
+    },
 });
-
-const User = mongoose.model('User', userSchema);
+userSchema.methods.createEmailToken = function () {
+    this.verify_token = randomstring.generate({
+        length: 10,
+    });
+    this.tokenExpires = Date.now() + 5 * 60 * 1000;
+    return;
+};
+const User = mongoose.model("User", userSchema);
 module.exports = User;
